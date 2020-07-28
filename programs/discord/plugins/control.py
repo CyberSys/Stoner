49b92789b7985c92614115f1c214b2beee1baed1
FWolfe <noreply@tekagis.ca>
2020-07-28 16:36:30 -0700

    basic bot control plugins

---
+++ programs/discord/plugins/control.py
@@ -0,0 +1,132 @@
+# -*- coding: utf-8 -*-
+
+from disco.bot import Plugin
+from disco.types.message import MessageEmbed
+from dogma.program import ProgramLoadError
+
+
+def embedReply(title=None, description=None, event=None):
+    embed = MessageEmbed()
+    embed.title = title
+    embed.description = description
+    embed.color = '10038562'
+    if event:
+        event.msg.reply(None, embed=embed)
+    return embed
+
+    
+class DogmaControl(Plugin):
+
+#    def load(self, ctx):
+#        super(DogmaControl, self).load(ctx)
+
+
+#    def unload(self, ctx):
+#        return super(DogmaControl, self).unload(ctx)
+
+
+#==============================================================================
+#
+#==============================================================================
+    def agent(self):
+        return self.parent.agent
+
+
+    @Plugin.command('load', '<unique_id:str>', group='program', level=1000)
+    def on_program_load_command(self, event, unique_id):
+        try:
+            config = self.agent().config_load(unique_id)
+            program = self.agent().program_import(
+                module=config["_module"],
+                unique_id=unique_id,
+                classname=config["_class"], 
+                config=config,
+                plugins=config.get('plugins')
+            )
+            program.init()
+            event.msg.reply("ok")
+        except ProgramLoadError as msg:
+            event.msg.reply(f"**ProgramLoadError** `{msg}`")
+
+
+    @Plugin.command('unload', '<unique_id:str>', group='program', level=1000)
+    def on_program_unload_command(self, event, unique_id):
+        try:
+            self.agent().program_unload(unique_id)
+            event.msg.reply("ok")
+        except ProgramLoadError as msg:
+            event.msg.reply(f"**ProgramLoadError** `{msg}`")
+            
+
+
+    @Plugin.command('reload', '<unique_id:str>', group='program', level=1000)
+    def on_program_reload_command(self, event, unique_id):
+        try:
+            self.agent().program_reload(unique_id)
+            event.msg.reply("ok")
+        except ProgramLoadError as msg:
+            event.msg.reply(f"**ProgramLoadError** `{msg}`")
+
+
+
+    @Plugin.command('list', group='program', level=1000)
+    def on_program_list_command(self, event):
+        event.msg.reply(str([x for x in self.bot.agent.programs.keys()]))
+
+
+
+    @Plugin.command('load', '<program:str> <plugin:str>', group='plugin', level=1000)
+    def on_plugin_load_command(self, event, program, plugin):
+        if program == 'discord':
+            try:
+                self.bot.add_plugin_module(plugin)
+                event.msg.reply("ok")
+            except ModuleNotFoundError as msg:
+                event.msg.reply(f"**ModuleNotFoundError** `{msg}`")
+            return
+
+        pro = self.agent().programs.get(program)
+        if not pro:
+            event.msg.reply(f"**BadInputError** `can't load plugin for non-loaded program {program}`")
+            return
+        if not hasattr(pro, 'plugin_import'):
+            event.msg.reply(f"**BadInputError** `program {program} is not a instance of PlugableProgram`")
+            return
+
+        try:
+            pro.plugin_import(plugin, pro.config.get('plugins', {}).get(plugin))
+            event.msg.reply("ok")
+        except ProgramLoadError as msg:
+            event.msg.reply(f"**ProgramLoadError** `{msg}`")
+
+        
+
+    @Plugin.command('unload', '<program:str> <plugin:str>', group='plugin', level=1000)
+    def on_plugin_unload_command(self, event, program, plugin):
+        pass
+
+
+
+    @Plugin.command('reload', '<program:str>  <plugin:str>', group='plugin', level=1000)
+    def on_plugin_reload_command(self, event, program, plugin):
+        pass
+
+
+    @Plugin.command('list', '<program:str>', group='plugin', level=1000)
+    def on_plugin_list_command(self, event, program):
+        if program == 'discord':
+            pro = self.bot
+        else:
+            pro = self.bot.agent.programs.get(program)
+        if pro:
+            event.msg.reply(str([x for x in pro.plugins.keys()]))
+
+
+    @Plugin.listen('MessageCreate')
+    def on_message_create(self, event):
+        if event.author.id == self.bot.parent.discord_id: # ignore ourself.
+            return
+        
+        access = self.bot.get_level(event.author)
+        if event.guild is None and access < 1000:
+            self.parent.notify("DM Received (%s)" % event.author, event.content)
---
+++ programs/irc/plugins/control.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov  5 14:36:09 2018
+
+@author: wolf
+"""
+
+import dogma.program
+PROGRAM_COMMANDS = ('list', 'load', 'unload','reload')
+
+
+class Plugin(dogma.program.Plugin):
+    access = {
+        'ev' : 1000,
+        'join' : 80,
+        'part' : 80,
+        'shutdown' : 1000,
+        'program' : 1000,
+        'plugin' : 1000,
+    }
+    def __init__(self, client):
+        super().__init__(client)
+        #Plugin.__init__(self, client)
+
+
+    def load(self, config=None, state=None):
+        super().load(config, state)
+        self.config = config
+
+
+    def _event_INVITE(self, event):
+        if event.network.getaccess(event.source) >= self.access['join']:
+            event.network.join(event.text)
+
+
+    def _command_join(self, event, args):
+        event.network.join(args)
+
+
+    def _command_part(self, event, args):
+        event.network.part(args)
+
+
+    def _command_shutdown(self, event, args):
+        self.agent().shutdown()
+
+
+    def _command_program(self, event, args):
+        try:
+            cmd, unique_id = args.split(None, 1)
+        
+        except ValueError:
+            cmd, unique_id = args, None
+
+        try:
+            if cmd == 'list':
+                event.reply(str(self.agent().programs.keys()))
+    
+            if cmd == 'load':
+                config = self.agent().config_load(unique_id)
+                if not config:
+                    raise dogma.program.ProgramLoadError("No config file for %s" % unique_id)
+                program = self.agent().program_import(
+                    module=config["_module"],
+                    unique_id=unique_id,
+                    classname=config["_class"], 
+                    config=config,
+                    plugins=config.get('plugins')
+                )
+
+                program.init()
+                event.reply("ok")
+
+            elif cmd == "unload":
+                self.agent().program_unload(unique_id)
+                event.reply("ok")
+            
+            elif cmd == "reload":
+                self.agent().program_reload(unique_id)
+                event.reply("ok")
+
+        except dogma.program.ProgramLoadError as msg:
+            event.reply(f"**ProgramLoadError** `{msg}`")
+
+
+    def _command_plugin(self, event, args):
+        try:
+            cmd, program, plugin = args.split(None, 2)
+        
+        except ValueError:
+            return
+
+        if not cmd in PROGRAM_COMMANDS:
+            event.reply(f"**BadInputError** {cmd} not in {PROGRAM_COMMANDS}`")
+            return
+
+        pro = self.uncle(program)
+        if not pro:
+            event.reply(f"**BadInputError** `can't {cmd} plugin for non-loaded program {program}`")
+            return
+
+        try:        
+            if cmd == 'list':
+                event.reply(str(self.parent.plugs.keys()))
+
+            elif cmd == 'load':
+                pro.plugin_import(plugin, pro.config.get('plugins', {}).get(plugin))
+                event.reply("ok")
+
+            elif cmd == "unload":
+                pro.plugin_unload(plugin)
+                event.reply("ok")
+
+            elif cmd == "reload":
+                pro.plugin_reload(plugin)
+                event.reply("ok")
+
+        except dogma.program.ProgramLoadError as msg:
+            event.reply(f"**ProgramLoadError** `{msg}`")
+
