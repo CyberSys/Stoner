8254fdf740a07156a4a6dcba3a759f16c9b2dabe
FWolfe <noreply@tekagis.ca>
2020-07-28 16:11:59 -0700

    added basic discord > irc bridge plugins

---
+++ programs/discord/plugins/irc_bridge.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+
+from disco.bot import Plugin
+import re
+text_filter = re.compile(r"[^A-Za-z0-9,\./ \~\!\@\#\$\%\^\&\*\(\)\_\-\+\=\|\[\]\{\}]")
+
+
+class IRCBridge(Plugin):
+    def load(self, ctx):
+        super(IRCBridge, self).load(ctx)
+        self.channel_map = {}
+        self.update_channels()
+    
+    def update_channels(self):
+        self.channel_map = {}
+
+        irc = self.bot.client.agent.programs.get('irc')
+        if not irc:
+            return
+        
+        bridge = irc.config.get('plugins',{}).get('discord_bridge', {}).get('connect',{})
+        for network in bridge:
+            for channel, id in bridge[network].items():
+                self.channel_map[id] = (network, channel)
+                print(id, network, channel)
+        
+
+
+    @Plugin.listen('MessageCreate')
+    def on_message_create(self, event):
+        if event.author.id == self.bot.parent.discord_id: # ignore ourself.
+            return
+        
+        if not event.channel_id or not event.channel_id in self.channel_map:
+            return
+        mapping = self.channel_map[event.channel_id]
+                
+        irc = self.bot.client.agent.programs.get('irc')
+        if not irc or not mapping[0] in irc.networks:
+            return
+        net = irc.networks[mapping[0]]
+        text = event.content.splitlines()
+        name = text_filter.sub('', event.author.username)
+        if not name:
+            name = event.author.id
+        text = ["%s (discord): %s" % (name, text_filter.sub('',  t)) for t in text]
+        net.privmsg(mapping[1], text)
+        
---
+++ programs/irc/plugins/discord_bridge.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+import dogma.program
+import re
+text_filter = re.compile(r"[^A-Za-z0-9,\./ \~\!\@\#\$\%\^\&\*\(\)\_\-\+\=\|\[\]\{\}]")
+
+class Plugin(dogma.program.Plugin):
+    def init(self):
+        disco = self.uncle('discord')
+        if not disco:
+            return
+        bridge = disco.bot.plugins.get('IRCBridge')
+        if not bridge:
+            return
+        bridge.update_channels()
+
+
+
+    def _event_PRIVMSG(self, event):
+        if not event.chan:
+            return 
+
+        id = self.config['connect'].get(event.network.name, {}).get(event.chan)
+        disco = self.uncle('discord')
+        if not id or not disco:
+            return
+
+        text = text_filter.sub("", event.text)
+        disco.client.api.channels_messages_create(id, "**%s** *(irc)*: `%s`" % (event.nick, text))
+
+
